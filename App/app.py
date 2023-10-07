4bbe28b3efaf2aacbcf1254f057c8a90fe68de8c
Danimelenro <119013225+Danimelenro@users.noreply.github.com>
2023-10-06 19:23:55 -0500

    Add files via upload

---
+++ App/app.py
@@ -0,0 +1,43 @@
+import pyodbc
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+# CONECTAR A LA BASE DE DATOS DESDE ACCESS
+conn = pyodbc.connect(r'DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=C:\Users\danim\.vscode\My project web\Data Bases\TasksDB.accdb;')
+cursor = conn.cursor()
+
+def conectar_bd():
+    return pyodbc.connect(conn)
+
+# RUTAS DE LA APLICACIÓN
+
+@app.route('/')
+def mostrar_tareas():
+    cursor.execute('SELECT * FROM Tareas')
+    tareas = cursor.fetchall()
+    return render_template('mostrar_tareas.html', tareas=tareas)
+
+@app.route('/agregar_tarea', methods=['POST'])
+def agregar_tarea():
+    if request.method == 'POST':
+        descripcion = request.form['descripcion']
+        estado = 'No Completado'  
+        cursor.execute("INSERT INTO Tareas (descripcion, estado) VALUES (?, ?)", (descripcion, estado))
+        conn.commit()
+    return redirect(url_for('mostrar_tareas'))
+
+@app.route('/completar_tarea/<int:id>')
+def completar_tarea(id):
+    cursor.execute("UPDATE Tareas SET estado='Completado' WHERE id=?", (id,))
+    conn.commit()
+    return redirect(url_for('mostrar_tareas'))
+
+@app.route('/eliminar_tarea/<int:id>')
+def eliminar_tarea(id):
+    cursor.execute("DELETE FROM Tareas WHERE id=?", (id,))
+    conn.commit()
+    return redirect(url_for('mostrar_tareas'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
